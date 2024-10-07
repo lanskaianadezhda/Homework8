03aaebb5d8b9cb674c30229f67720fb5b1aa5dbb
lanskaianadezhda <nadin-91@bk.ru>
2024-10-07 14:52:51 +0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
         weights[0] = 1;
         weights[1] = 2;
         weights[2] = 3;
-        double [] numbers = {1.57 , 7.654, 9.986};
-        int[] mounths = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12};
+        double[] numbers = {1.57, 7.654, 9.986};
+        int[] mounths = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
         // task 2
         System.out.println("Задача 2");
         for (int i = 0; i < 3; i++) {
@@ -24,20 +24,24 @@ public class Main {
         for (int i = weights.length - 1; i >= 0; i--) {
             System.out.print(weights[i]);
             if (i > 0) {
-                System.out.print(" , "); System.out.println();
+                System.out.print(" , ");
+            }
         }
+        System.out.println();
         for (int i = numbers.length - 1; i >= 0; i--) {
             System.out.print(numbers[i]);
             if (i > 0) {
-                System.out.print(" , ");System.out.println();
+                System.out.print(" , ");
             }
         }
+        System.out.println();
         for (int i = mounths.length - 1; i >= 0; i--) {
             System.out.print(mounths[i]);
             if (i > 0) {
                 System.out.print(" , ");
-                System.out.println();
             }
         }
+        System.out.println();
+
     }
 }
